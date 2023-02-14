6255b2c6b8b93ebc9ed23251730b128f004092b6
Solène <solene.gouin@orange.fr>
2023-02-14 14:50:58 +0100

    commit

---
+++ src/accueil/Accueil.jsx
@@ -0,0 +1,24 @@
+import './accueil.css';
+
+const Accueil = () => {
+
+    return (
+        <section className="home">
+            <div className="home__image">
+                <img src="/src/assets/germaine.jpg" alt="Germaine ROZ" />
+            </div>
+            <h3>Germaine ROZ</h3>
+            <p>Agent Top Secret</p>
+            <article>
+                Germaine est un monstre fort peu sympathique, gardienne de l'usine et responsable des dossiers dans Monstres et Cie.
+                <br />C'est une secrétaire à cheval sur les détails et les principes.
+                <br />Elle parle très lentement et bouge à la vitesse d’un escargot.
+                <br />Cependant, elle a une vivacité d’esprit étonnante pour une limace.
+                <br />Elle reproche constamment à Bob de ne pas lui rendre ses rapports en temps et en heure. 
+                <br />Germaine est un personnage pénible, froid et aigri.
+            </article>
+        </section>
+    )
+}
+
+export default Accueil;
---
+++ src/experience/Experience.jsx
@@ -0,0 +1,17 @@
+import './experience.css';
+
+const Experience = () => {
+
+    return (
+        <section className="experience">
+            <article>Secrétaire</article>
+            <article>Responsable des équipes des portes</article>
+            <article>Gardienne de l'usine</article>
+            <article>Agent de la CDA</article>
+            <article>Miss Monstropolis de 1950</article>
+            <article>Bully de Bob Razowsky n°1</article>
+        </section>
+    )
+}
+
+export default Experience;
